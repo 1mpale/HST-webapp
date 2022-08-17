0bfb97eee42c091654d4052bd1df1a517fd38f09
Vladimir Pain <vladimir.pain@sentia.com>
2022-08-17 18:24:55 +0200

    improved and fixed azure

--- pages/views.py
+++ pages/views.py
@@ -20,12 +20,14 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from django.http import HttpResponse
 import matplotlib
-from azure.storage.blob import ContainerClient
-import yaml
 import os
 from azure.core.credentials import AzureNamedKeyCredential
 from azure.data.tables import TableServiceClient
 from azure.storage.blob import BlobClient
+from azure.data.tables import TableClient
+from datetime import timedelta
+from azure.storage.blob import BlobClient, generate_blob_sas, BlobSasPermissions
+from urllib.request import urlopen
 
 
 @api_view(['GET'])
@@ -38,20 +40,26 @@ def index(request):
 
 @api_view(["POST"])
 def prediction(request):
+    account_name = ''
+    account_key = ''
+    connection_string = ''
     try:
-        credential = AzureNamedKeyCredential("STORAGENAME", "ACESS KEY")
-        service = TableServiceClient(endpoint="https://STORAGENAME.table.core.windows.net/", credential=credential)
+        credential = AzureNamedKeyCredential(account_name, account_key)
+        service = TableServiceClient(endpoint="https://vladimirstoragethesis.table.core.windows.net/", credential=credential)
         string = request.data
         string = "".join(string.values())
         scores_list = []
         HST = PagesConfig.HST
+        time = str(datetime.datetime.now())
+        id = str(uuid.uuid4())
         if base64.b64encode(base64.b64decode(string)).decode("utf-8") == string:
             data_for_validation = base64.b64decode(string)
             data_for_validation = data_for_validation.decode("utf-8")
-            with open('decoded.csv', 'w') as out:
+            fullname = os.path.join('./csvs/', id+".decoded.csv")
+            with open(fullname, 'w') as out:
                 out.write(data_for_validation)
             data_stream = stream.iter_csv(
-                'decoded.csv',
+                fullname,
                 converters={
                     'name': int,
                     'id': int,
@@ -68,26 +76,27 @@ def prediction(request):
             summ_of_scores = sum(scores_list)
             average_score = summ_of_scores/length_of_list
 
-            table_service_client = TableServiceClient.from_connection_string(conn_str="CONNECTION STRING")
+            table_service_client = TableServiceClient.from_connection_string(conn_str=connection_string)
             #table_name = "scoreresults"
             #table_client = table_service_client.create_table(table_name=table_name)
-
-            time = str(datetime.datetime.now())
-            id = str(uuid.uuid4())
             
-            blob = BlobClient.from_connection_string(conn_str="CONNECTION STRING", container_name="NAME OF THE CONTAINER", blob_name=id)
+            blob = BlobClient.from_connection_string(conn_str=connection_string, container_name="imagestorage", blob_name=id)
             blob.upload_blob(string)
 
-            base64string = 'https://myaccount.blob.core.windows.net/NAMEOF THE BLOB/' + 'id'
-            
+            blob = BlobClient.from_connection_string(conn_str=connection_string, container_name="fullscoreliststorage", blob_name=id)
+            blob.upload_blob(str(scores_list))
+
+            base64string = 'https://vladimirstoragethesis.blob.core.windows.net/imagestorage/' + id
+            full_score_list = 'https://vladimirstoragethesis.blob.core.windows.net/fullscoreliststorage/' + id
             my_entity = {
-                u'PartitionKey':id,
-                u'RowKey': time,
+                u'PartitionKey':time,
+                u'RowKey': id,
                 u'Average_Score': average_score,
-                u'Base64': base64string
+                u'Base64': base64string,
+                u'Full_Score_list': full_score_list
             }
-            table_service_client = TableServiceClient.from_connection_string(conn_str="CONNECTION STRING")
-            table_client = table_service_client.get_table_client(table_name="TABLE NAME")
+            table_service_client = TableServiceClient.from_connection_string(conn_str=connection_string)
+            table_client = table_service_client.get_table_client(table_name="scoreresults")
             table_client.create_entity(entity=my_entity)
 
 
@@ -118,73 +127,70 @@ def visualization(request):
     ax.set_zlabel("x_composite_3")
     scores_list = []
     HST = PagesConfig.HST 
-    path = Path(os.getcwd())
-    for file in path.glob('*.csv'):
-        data_stream = stream.iter_csv(
-            file,
-            converters={
-                'name': int,
-                'id': int,
-                'self_size': int,
-                'edge_count': int,
-                'trace_node_id': int,
-                'detachedness': int,
-            }
-        )
-        for x, y in data_stream:
-            score = HST.score_one(x)
-            scores_list.append(score)
-        df = pd.read_csv(file)
-        df['anomaly_score'] = scores_list
-        outliers=df.loc[df['anomaly_score']>0.99]
-        outlier_index=list(outliers.index)
-        pca = PCA(n_components=3)  # Reduce to k=3 dimensions
-        scaler = StandardScaler()
-        to_model_columns=df.columns[1:6]
-        X = scaler.fit_transform(df[to_model_columns])
-        X_reduce = pca.fit_transform(X)
-        # Plot the compressed data points
-        ax.scatter(X_reduce[:, 0], X_reduce[:, 1], zs=X_reduce[:, 2], s=4, lw=1, label="inliers",c="green")
-        # Plot x's for the ground truth outliers
-        ax.scatter(X_reduce[outlier_index,0],X_reduce[outlier_index,1], X_reduce[outlier_index,2],
-            lw=2, s=60, marker="x", c="red", label="outliers")
-        plt.draw
-        date = str(datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
-        plt.savefig(date+".visualized.png")
-        scores_list.clear() 
+    path = Path('./csvs')
 
-    image_data = open(date+".visualized.png", "rb").read()
+    def sort_files():
+        dir_name = Path('./csvs')
+    # Get list of all files only in the given directory
+        list_of_files = filter(lambda x: os.path.isfile(os.path.join(dir_name, x)),
+                           os.listdir(dir_name))
+    # Sort list of files based on last modification time in ascending order
+        list_of_files = sorted(list_of_files,
+                           key=lambda x: os.path.getmtime(os.path.join(dir_name, x)),reverse=True
+                           )
+        lastfile= list_of_files[-100:]
+        return lastfile
+    last100 = sort_files()
+    for file in path.glob('*.csv'):
+        if file.name in last100:
+            data_stream = stream.iter_csv(
+            file,
+                converters={
+                    'name': int,
+                    'id': int,
+                    'self_size': int,
+                    'edge_count': int,
+                    'trace_node_id': int,
+                    'detachedness': int,
+                }
+            )
+            for x, y in data_stream:
+                score = HST.score_one(x)
+                scores_list.append(score)
+            df = pd.read_csv(file)
+            df['anomaly_score'] = scores_list
+            outliers=df.loc[df['anomaly_score']>0.99]
+            outlier_index=list(outliers.index)
+            pca = PCA(n_components=3)  # Reduce to k=3 dimensions
+            scaler = StandardScaler()
+            to_model_columns=df.columns[1:6]
+            X = scaler.fit_transform(df[to_model_columns])
+            X_reduce = pca.fit_transform(X)
+            # Plot the compressed data points
+            ax.scatter(X_reduce[:, 0], X_reduce[:, 1], zs=X_reduce[:, 2], s=4, lw=1, label="inliers",c="green")
+            # Plot x's for the ground truth outliers
+            ax.scatter(X_reduce[outlier_index,0],X_reduce[outlier_index,1], X_reduce[outlier_index,2],
+                lw=2, s=60, marker="x", c="red", label="outliers")
+            plt.draw
+            date = str(datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
+            scores_list.clear() 
+    pathtosaveimage = os.path.join('./currentimage/', date+".visualized.png")
+    plt.savefig(pathtosaveimage)
+    image_data = open(pathtosaveimage, "rb").read()
     #with open("visualized.png", "rb") as image_file:
         #encoded_string = base64.b64encode(image_file.read())
 
-    def load_config():
-        path = os.getcwd()
-        dir_root = os.path.join(path, r"uploadconfig.yaml")
-        with open(dir_root, "r") as yamlfile:
-            return yaml.load(yamlfile, Loader=yaml.FullLoader)
-    config = load_config()    
-
-    def get_files(dir):
-        with os.scandir(dir) as entries:
-            for entry in entries:
-                if entry.is_file() and entry.name.endswith('png'):
-                    yield entry   
-
-    pictures = get_files(config["source_folder"])
     
-    def upload(files, connection_string, container_name):
-        container_client = ContainerClient.from_connection_string(connection_string, container_name)    
-        for file in files:
-            blob_client = container_client.get_blob_client(file.name)
-            with open(file.path,"rb") as data:
-                blob_client.upload_blob(data)
-                path = os.getcwd()
-                archive = os.path.join(path, r"archive")
-                destination = os.path.join(archive, file.name)
-                os.replace(file.path, destination)
-    upload(pictures, config["azure_storage_connectionstring"], config["image_container_name"])
+            #table_name = "scoreresults"
+            #table_client = table_service_client.create_table(table_name=table_name)
+    blob = BlobClient.from_connection_string(conn_str="DefaultEndpointsProtocol=https;AccountName=vladimirstoragethesis;AccountKey=XdyCazpFWpyUs+iuKkS2yD4GZ+8GaAram/ltSwv2C8uG/hi7dvVKcFE9F3uGaqcbB7tAYWdUgIgY+AStxuPnQQ==;EndpointSuffix=core.windows.net", container_name="visualizationimages", blob_name=date+".visualized.png")
+    with open(pathtosaveimage, "rb") as data:
+        blob.upload_blob(data)
+
+    destination = os.path.join('./archive', date+".visualized.png")
+    os.replace(pathtosaveimage, destination)
+
+    
     
  
     return HttpResponse(image_data, content_type="image/png")  
-
-
